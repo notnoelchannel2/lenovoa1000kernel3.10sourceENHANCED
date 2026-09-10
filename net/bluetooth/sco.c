58b47f2e1416aaae6bdaf423d6021bb88fe8c49d
nnc <notnoelchannel1@protonmail.com>
2026-09-10 20:32:29 +0200

    Update sco.c

--- net/bluetooth/sco.c
+++ net/bluetooth/sco.c
@@ -448,13 +448,13 @@ static int sco_sock_create(struct net *net, struct socket *sock, int protocol,
 	return 0;
 }
 
-static int sco_sock_bind(struct socket *sock, struct sockaddr *addr, int alen)
+static int sco_sock_bind(struct socket *sock, struct sockaddr *addr, int addr_len)
 {
-	struct sockaddr_sco sa;
+	struct sockaddr_sco *sa = (struct sockaddr_sco *) addr;
 	struct sock *sk = sock->sk;
-	int len, err = 0;
+	int err = 0;
 
-	BT_DBG("sk %p %pMR", sk, &sa.sco_bdaddr);
+	BT_DBG("sk %p %pMR", sk, &sa->sco_bdaddr);
 
 	if (!addr || addr->sa_family != AF_BLUETOOTH)
 		return -EINVAL;
@@ -474,8 +474,8 @@ static int sco_sock_bind(struct socket *sock, struct sockaddr *addr, int alen)
 		goto done;
 	}
 
-	bacpy(&bt_sk(sk)->src, &sa.sco_bdaddr);
-	sco_pi(sk)->pkt_type = sa.sco_pkt_type;
+	bacpy(&bt_sk(sk)->src, &sa->sco_bdaddr);
+	sco_pi(sk)->pkt_type = sa->sco_pkt_type;
 
 	sk->sk_state = BT_BOUND;
 
